baf33b9a2a11be79aed59d6137a8b5527e9953fa
Kevin Nobel <hi@2sk.nl>
2020-06-19 21:07:38 +0200

    Basic plugin.

---
+++ src/main/java/net/luniker/lunifix/LuniFix.java
@@ -0,0 +1,10 @@
+package net.luniker.lunifix;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LuniFix extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+}
